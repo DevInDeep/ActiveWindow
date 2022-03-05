36223a51ea1a53be0cb7b7401c6664293bf60387
vpavlevski <vpavlevski@yahoo.com>
2022-03-05 14:27:36 +0100

    Active Window

---
+++ ActiveWindow/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ActiveWindow.BLL.ActiveWindow;
+
+namespace ActiveWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var activeWindowWatcher = new ActiveWindowWatcher(TimeSpan.FromMilliseconds(500));
+            activeWindowWatcher.ActiveWindowChanged += (o, e) => Console.WriteLine($"Active Window: {e.ActiveWindow}");
+            activeWindowWatcher.Start();
+            
+            Console.ReadLine();
+        }
+    }
+}
